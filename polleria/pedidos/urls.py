5aa4f8a4896ac9844cb22eda5c39164655b08fb6
Jherson <jinca@unsa.edu.pe>
2024-06-28 20:16:53 -0500

    añadir las vistas de subir datos al path

--- polleria/pedidos/urls.py
+++ polleria/pedidos/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('crear_pollo/', views.crear_pollo, name='crear_pollo'),
+    path('crear_bebida/', views.crear_bebida, name='crear_bebida'),
+    path('crear_crema/', views.crear_crema, name='crear_crema'),
+    path('crear_papa/', views.crear_papa, name='crear_papa'),
 ]
