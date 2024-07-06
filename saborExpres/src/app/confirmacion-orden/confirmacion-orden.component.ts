e928ab161844f5dd2aa9b13a555e9bec5967fef3
Jherson <jinca@unsa.edu.pe>
2024-07-06 18:50:56 -0500

    generando servicios cart, y corrigiendo el nombre del componente confirmation-orden, y añadir el servicio del carrito mientras

---
+++ saborExpres/src/app/confirmacion-orden/confirmacion-orden.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmacion-orden',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirmacion-orden.component.html',
+  styleUrl: './confirmacion-orden.component.css'
+})
+export class ConfirmacionOrdenComponent {
+
+}
