7b8f529f77348d2da9061c9f30dfdea2c169aacf
Jherson <jinca@unsa.edu.pe>
2024-07-06 19:04:40 -0500

    componente menu

--- saborExpres/src/app/menu/menu.component.ts
+++ saborExpres/src/app/menu/menu.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+// menu.component.ts
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { ApiService } from '../api.service';
+import { AuthService } from '../auth.service';
+
+interface Producto {
+  id: any;
+  nombre: string;
+  descripcion: string;
+  precio: string;
+  categoria: string | null;
+  imagen_url: string | null;
+  cantidad: number;  // Añadir cantidad aquí
+}
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule, HttpClientModule],
+  providers: [ApiService],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  productos: Producto[] = [];
+  carrito: { producto_id: number, cliente_id: number }[] = [];
+  clienteId = 1;  // ID del cliente
 
+  constructor(private apiService: ApiService, private authService: AuthService) {
+    this.clienteId = this.authService.getClienteId();
+    if (this.clienteId === 0) {
+      this.clienteId = 4;  // Default value if not set
+    }
+  }
+
+  ngOnInit(): void {
+    this.loadProductos();
+  }
+
+  loadProductos(): void {
+    this.apiService.getProductos().subscribe(
+      data => {
+        this.productos = data;
+        console.log('Productos recibidos:', data);
+      },
+      error => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
+
+  showProductId(productId: number, clienteId: number): void {
+    alert(`ID del producto: ${productId}, ID del cliente: ${clienteId}`);
+    this.apiService.agregarProductoAlCarrito(clienteId, productId).subscribe(
+      response => {
+        console.log('Producto agregado al carrito');
+        alert('Producto agregado al carrito');
+      },
+      error => {
+        console.error('Error adding product to cart:', error);
+      }
+    );
+  }
 }
