db65e6730e8c42c0c5152f4f68ed4f82f3fb294b
Jherson <jinca@unsa.edu.pe>
2024-07-06 19:04:07 -0500

    componente nav-bar

--- saborExpres/src/app/nav-bar/nav-bar.component.ts
+++ saborExpres/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+// nav-bar.component.ts
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.css'
+  styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  clienteNombre: string | null = null;
+  isLoggedIn: boolean = false;
 
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.authStatus$.subscribe(status => {
+      this.isLoggedIn = status;
+      this.clienteNombre = this.authService.getClienteNombre();
+    });
+
+    // Inicializar los valores
+    this.isLoggedIn = !!localStorage.getItem('token');
+    this.clienteNombre = localStorage.getItem('clienteNombre');
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
