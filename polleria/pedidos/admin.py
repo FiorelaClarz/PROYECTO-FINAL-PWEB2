d902943dbe47d57853013f585d6e2df84a941b23
Jherson <jinca@unsa.edu.pe>
2024-07-09 16:34:28 -0500

    borrando carpeta

--- polleria/pedidos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Pollo,Papa,Bebida,Crema
-admin.site.register(Pollo)
-admin.site.register(Papa)
-admin.site.register(Bebida)
-admin.site.register(Crema)
